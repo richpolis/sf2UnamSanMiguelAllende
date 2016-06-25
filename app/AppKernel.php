83c76b92d7647cc31317c4fd03ec4b9e55a54569
Richpolis Systems <richpolis@gmail.com>
2016-06-25 11:04:09 -0500

    agregando fixtures para prod

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,13 +19,19 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new UNAM\AppBundle\UNAMAppBundle(),
         );
+        
+        // esta linea se eliminara despues de cargar los fixtures
+        if (in_array($this->getEnvironment(), array('prod'))) {
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        }
+        
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-			$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
